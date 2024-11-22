f4ad11efc0ef030a39276df7ef948c30cbc036e4
Sri Devi Wijaya <sri.deviwijaya@gmail.com>
2024-11-22 09:58:01 +0800

    first commit

--- download_ds.py
+++ download_ds.py
@@ -1,10 +1,11 @@
 from datasets import load_dataset, load_from_disk
 import sys
 
-dataset = load_dataset("wikitext", "wikitext-103-v1", split="train")
-dataset = load_from_disk(data_dir)
-text_data = " ".join(dataset["text"])
-dataset.save_to_disk("./wikitext-103-v1-train")
+# dataset = load_dataset("wikitext", "wikitext-103-v1", split="train")
+# text_data = " ".join(dataset["text"])
+# dataset.save_to_disk("./wikitext-103-v1-train")
+
+dataset = load_dataset("bookcorpus", trust_remote_code=True)
+dataset.save_to_disk("./bookcorpus")
 
 print("dataset saved")
-sys.exit(0)
